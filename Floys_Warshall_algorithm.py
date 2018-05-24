9cf73a87c9393e4187445f8fcb55084ff4489ecc
csakoji <39510624+csakoji@users.noreply.github.com>
2018-05-24 17:45:11 +0530

    Added comments

--- Floys_Warshall_algorithm.py
+++ Floys_Warshall_algorithm.py
@@ -1,14 +1,19 @@
 from math import inf
 
+# Input graph for algorithm
 alist = [[0,5,inf,10],
-		[inf,0,3,inf],
-		[inf,inf,0,1],
-		[inf,inf,inf,0]
-		]
+	[inf,0,3,inf],
+	[inf,inf,0,1],
+	[inf,inf,inf,0]
+	]
 
+# Iteration number
 interation = 0
+
+# Loop till iterations equal to number of vertices is done
 while interation < len(alist):
 	# print("iteration is {}".format(interation))
+	# Iterate over each row and column of input graph except row==col and (row or col) == iteration  
 	for row in range(0, len(alist)):
 		if row == interation:
 			continue
@@ -20,8 +25,13 @@ while interation < len(alist):
 			# print("alist[row][col] is {}".format(alist[row][col]))
 			# print("alist[row][iteration] is {}".format(alist[row][interation]))
 			# print("alist[col][iteration] is {}".format(alist[col][interation]))
+			
+			# Check if the value of edge between any two vertices of graph is greater than 
+			# distance between those two vertices via "iteration" vertex
 			if alist[row][col] > (alist[row][interation] + alist[col][interation]):
+				# If true them modify the distance value between those two vertices
 				alist[row][col] = (alist[row][interation] + alist[col][interation])
+	# Increament the iteration vertex to next vertex
 	interation += 1
 
 print(alist)
