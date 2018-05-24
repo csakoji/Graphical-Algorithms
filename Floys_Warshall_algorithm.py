9f06910f6a32c5f017efb2689ea8b80284fb6fdd
csakoji <39510624+csakoji@users.noreply.github.com>
2018-05-24 08:25:11 +0530

    Update Floys_Warshall_algorithm.py
    Working successfully

--- Floys_Warshall_algorithm.py
+++ Floys_Warshall_algorithm.py
@@ -1,29 +1,27 @@
 from math import inf
 
-alist = [[0,5,1,2],
-		[5,0,3,inf],
-		[1,3,0,4],
-		[2,inf,0,4]
+alist = [[0,5,inf,10],
+		[inf,0,3,inf],
+		[inf,inf,0,1],
+		[inf,inf,inf,0]
 		]
 
 interation = 0
 while interation < len(alist):
-	print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-	print("iteration is {}".format(interation))
+	# print("iteration is {}".format(interation))
 	for row in range(0, len(alist)):
 		if row == interation:
 			continue
 		for col in range(0, len(alist[row])):
 			if col == interation or col == row:
 				continue
-			print("row is {}".format(row))
-			print("col is {}".format(col))
-			print("alist[row][col] is {}".format(alist[row][col]))
-			print("alist[row][iteration] is {}".format(alist[row][interation]))
-			print("alist[col][iteration] is {}".format(alist[col][interation]))
+			# print("row is {}".format(row))
+			# print("col is {}".format(col))
+			# print("alist[row][col] is {}".format(alist[row][col]))
+			# print("alist[row][iteration] is {}".format(alist[row][interation]))
+			# print("alist[col][iteration] is {}".format(alist[col][interation]))
 			if alist[row][col] > (alist[row][interation] + alist[col][interation]):
 				alist[row][col] = (alist[row][interation] + alist[col][interation])
-			print(alist)
 	interation += 1
 
 print(alist)
